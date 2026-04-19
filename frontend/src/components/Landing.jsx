a66d2b3f51693cdaba26a3e10f77921175062080
Andrew Senkowski <51846520+AndrewHSenko@users.noreply.github.com>
2026-04-19 15:38:56 -0400

    Landing done

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,27 +1,29 @@
 import landing from '../assets/landing.png'
-import intro from '../assets/landing_home.jpeg'
+import intro from '../assets/intro.png'
 
 const Landing = () => {
   return (
     <div className="container-fluid">
-        <div className="row">
+        <div className="row"> {/** Landing */}
             <div class="card text-center text-dark p-0 border-0" >
                 <img src={landing} class="card-img img-fluid" alt="House by https://unsplash.com/illustrations/modern-white-building-with-blue-sky-and-shadows-Jbr4MUg4cMA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" />
                 <div class="card-img-overlay align-self-start text-dark pt-1">
-                    <h1 className="card-title display-lg-1 display-md-3 display-sm-5 fw-bold pt-xl-4 pt-lg-2 mb-0">Marketing Solution VI</h1>
+                    <h1 className="card-title display-lg-1 display-md-3 display-sm-5 fw-bold pt-xxl-5 pt-xl-4 pt-lg-2 mb-0">Marketing Solution VI</h1>
                     <hr className="d-none d-md-block border-1 border-primary-dark opacity-75 w-25 mx-auto py-xl-2"></hr>
-                    <p className="display-lg-5 text-muted fs-sm-3 fs-5"><i>The</i> HOA Marketing Solution</p>
-
-                    
+                    <p className="display-lg-5 text-muted fs-sm-3 fs-5 pt-2 pt-md-0"><i>The</i> HOA Marketing Solution</p>
                 </div>
             </div>
         </div>
-        <div className="row">
-            <div className="col-12 col-md-6">
-                <p>Hello</p>
+        <div className="row"> {/** Intro */}
+            <div className="col-12 col-md-6 d-flex flex-column justify-content-center">
+                <div class="card border-0 d-flex flex-column gap-md-5 gap-4 p-0 ms-lg-2 ms-md-4 mx-xl-5 text-dark">
+                    <h1 className="card-title mb-0 pt-sm-2 display-xl-3 display-lg-5 fs-1 lead text-md-start text-center">Broaden your horizons and reach new clients with Marketing Solution VI</h1>
+                    <img src={intro} className="intro img-fluid w-75 d-block d-md-none mx-auto" alt="House by Alghozy (https://unsplash.com/@artgho/illustrations?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)"/>
+                    <p className="fs-6 fs-lg-5 lead">With our CID List, you can find new clients, stay connected with old clients, and help scale your business! Reach out directly to the HOAs you want, market to new counties, and see their information on a map through Mapline"</p>
+                </div> 
             </div>
-            <div className="col-12 col-md-6">
-                <img src={intro} className="img-fluid" alt="House by Alghozy (https://unsplash.com/@artgho/illustrations?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)"/>
+            <div className="col-12 col-md-6 px-0">
+                <img src={intro} className="intro img-fluid w-75 d-none d-md-block ms-auto" alt="House by Alghozy (https://unsplash.com/@artgho/illustrations?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)"/>
             </div>
         </div>
         
