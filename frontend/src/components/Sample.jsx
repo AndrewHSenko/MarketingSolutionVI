a6c140227ddda55db06e8cc0d09df8ad4b6939b4
Andrew Senkowski <51846520+AndrewHSenko@users.noreply.github.com>
2026-05-22 00:07:50 -0400

    Footer and sample changes

--- frontend/src/components/Sample.jsx
+++ frontend/src/components/Sample.jsx
@@ -1,9 +1,9 @@
-import sample from "../assets/sample_sheet.jpeg";
+import sample from "../assets/sample_sheet.png";
 
 const Sample = ({isActive}) => {
   return (
-    <div className="d-flex justify-content-center align-items-center h-100">
-      <div class="card text-dark p-0 w-88 mx-auto">
+    <div className="d-flex justify-content-center align-items-center h-100 px-5">
+      <div class="card text-dark p-0 px-5 w-88 mx-auto">
           <img src={sample} class="card-img" alt="Sample spreadsheet" />
           <div class="card-img-overlay p-0 d-md-none d-flex justify-content-center align-items-center h-100 bg-primary bg-opacity-75">
               <button className="bg-primary p-3 w-100">
@@ -11,7 +11,7 @@ const Sample = ({isActive}) => {
                 <p class="card-text"><small>Click here to download!</small></p>
               </button>
           </div>
-          <button class={`card-img-overlay p-0 d-md-flex d-none justify-content-center align-items-center h-100 bg-primary ${isActive === "sample" ? "bg-opacity-75" : "opacity-0"}`}>
+          <button class={`card-img-overlay p-0 d-md-flex d-none justify-content-center align-items-center h-100 bg-primary ${isActive === 'sample' ? "bg-opacity-75" : "bg-opacity-25"}`}>
             <div className="bg-primary p-3 w-100 ">
                 <h4 class="lead fs-3 card-title">Sample Marketing Solution VI CID List</h4>
                 <p class="card-text"><small>Click here to download!</small></p>
--- frontend/src/components/Samples.jsx
+++ frontend/src/components/Samples.jsx
@@ -9,7 +9,7 @@ const Samples = () => {
         <div className="container-fluid my-4">
             <div className="row d-flex justify-content-center mb-3">
                 <div
-                    className={`col-12 col-lg-6 align-items-center  ${active === "sample" ? "bg-primary" : ""}`}
+                    className="col-12 align-items-center px-5"
                     onMouseEnter={() => setActive("sample")}
                     onMouseLeave={() => setActive(null)}
                 >
@@ -17,7 +17,8 @@ const Samples = () => {
                 </div>
             </div>
             <div className="row d-flex justify-content-center mt-3">
-                <iframe className="border border-2 border-primary-dark px-0" src="https://app.mapline.com/map/map_17a6146c/UlRaYVQzSno3Mno1eVJsd1BkVVlLUTdQbk9hUldwTll5TU5Vck" style={{width:"80%", height: "600px"}} allow="geolocation *"></iframe>
+                <h1 className="display-6 text-center py-3">Sample Marketing Solution VI CID Map</h1>
+                <iframe className="border border-2 border-primary-dark px-0" src="https://app.mapline.com/map/map_17a6146c/UlRaYVQzSno3Mno1eVJsd1BkVVlLUTdQbk9hUldwTll5TU5Vck" style={{width: "70%", height: "600px"}} allow="geolocation *"></iframe>
                 <div className="d-flex justify-content-center" style={{"font-size": "10px"}}>
                     <a href="https://mapline.com" target="_blank">Mapping by Mapline</a>
                 </div>
